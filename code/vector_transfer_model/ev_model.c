02d0db76c5b512d266b999eb154467887c54c096
codeskiii <piotrpawelec0000@gmail.com>
2024-09-23 00:04:45 +0200

    populating

--- code/vector_transfer_model/ev_model.c
+++ code/vector_transfer_model/ev_model.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "structs.c"
-
-/*
-<returns>
-    A new robot organism.
-</returns>
-*/
-struct robot_organism *create_robot_organism() {
-    struct robot_organism *currentRobot = malloc(sizeof(struct robot_organism));
-    currentRobot->goal = (struct vec3) {rd_num(-10, 10), rd_num(0, 10), rd_num(-10, 10)};
-    for (int i = 0; i < 7; i++) {
-        // simple wage init
-        currentRobot->wages[i] = 1;
-    }
-    return currentRobot;
-}
 
 /*
 <param name="min" type="int">
@@ -28,8 +14,35 @@ struct robot_organism *create_robot_organism() {
     A random number between min and max.
 </returns>
 */
-int rd_num(int min, int max) {
+int rd_int(int min, int max) {
     return rand() % (max - min + 1) + min;
+}
+
+/*
+<param name="max" type="float">
+    The maximum value.
+</param>
+<returns>
+    A random number between 0 and max.
+</returns>
+*/
+float *rd_float(float max) {
+    float result = ((float)rand() / (float)RAND_MAX) * max;
+    return &result;
+}
+
+/*
+<returns>
+    A new robot organism.
+</returns>
+*/
+struct robot_organism *create_robot_organism() {
+    struct robot_organism *currentRobot = malloc(sizeof(struct robot_organism));
+    currentRobot->goal = (struct vec3) {rd_int(-10, 10), rd_int(0, 10), rd_int(-10, 10)};
+    for (int i = 0; i < 7; i++) {
+        currentRobot->wages[i] = rd_float(1.0);
+    }
+    return currentRobot;
 }
 
 /*
@@ -37,14 +50,16 @@ int rd_num(int min, int max) {
     A new population.
 </returns>
 */
-struct population *create_population() {
+struct population *create_not_inherited_population() {
+    srand(time(NULL));
     struct population *currentPopulation = malloc(sizeof(struct population));
     for (int i = 0; i < 100; i++) {
         currentPopulation->collector[i] = create_robot_organism();
     }
+    return currentPopulation;
 }
 
-int main() {
-    srand(time(NULL));
-    return 0;
+// TO DO
+struct population *create_inherited_population(struct population *parent) {
+
 }
