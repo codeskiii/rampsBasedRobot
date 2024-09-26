9e531bd0342a456827ae3407133617a58d086311
codeskiii <piotrpawelec0000@gmail.com>
2024-09-26 23:58:33 +0200

    Precitions update

--- code/vector_transfer_model/predict_on_wages.c
+++ code/vector_transfer_model/predict_on_wages.c
@@ -4,9 +4,25 @@
 #include <math.h>
 #include "structs.c"
 
-// TO DO
-float calculate_wages(float base, float *wages[6]) {
-    
+/*
+<param name="base" type="float">
+    The X value
+</param>
+<param name="wages" type="float *">
+    wages for prediction making
+</param> 
+*/
+
+float *calculate_wages(float base, float *wages[6]) {
+    /*
+    X ** W_1 + X * W_2 ... = y
+    */
+   float result = 0;
+   for (int i = 0; i < 6; i++) {
+        result += powf(base, *wages[i]);
+   }
+
+   return &result;
 }
 
 /*
@@ -14,7 +30,7 @@ float calculate_wages(float base, float *wages[6]) {
     The robot with any wages.
 </param>
 <returns>
-    The prediction.
+    The prediction of vec6 wich is the question.
 </returns>
 */
 struct vec6 make_predictions_on_wages(struct robot_organism *robot) {
@@ -27,8 +43,11 @@ struct vec6 make_predictions_on_wages(struct robot_organism *robot) {
     struct vec3 *goal_vector = &robot->goal; 
 
     for (int i = 0; i < 6; i++) {
+        float cup_holder = 0; 
         for (int k = 0; k < 3; k++) {
-            result->degree[i] = calculate_wages(goal_vector->degree[k], robot->wages[i]);   
+            cup_holder += *calculate_wages(goal_vector->degree[k], robot->wages[i]);
         }
+        result->degree[i] = cup_holder;
     }
+    return *result;
 }
